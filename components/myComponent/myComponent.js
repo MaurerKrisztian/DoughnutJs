805ad41658d7c9845dc0b604934ca7c69fd496b1
Maurer Krisztian <maurerkrisztian>
2021-10-04 21:46:30 +0200

    components

---
+++ components/myComponent/myComponent.js
@@ -0,0 +1,10 @@
+import ComponentBase from "../../lib/ComponentBase.js";
+
+export class MyComponent extends ComponentBase {
+    constructor() {
+        super();
+
+        console.log("MyComponent")
+
+    }
+}
---
+++ components/test/Test.js
@@ -0,0 +1,7 @@
+import ComponentBase from "../../lib/ComponentBase.js";
+
+export class Test extends ComponentBase {
+    constructor() {
+        super();
+    }
+}
